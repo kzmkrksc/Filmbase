76d17ef7d9a7ae53bdcfe37151734e0f8b6fad51
Ahmet K. KARAKUSCU <22945971+kzmkrksc@users.noreply.github.com>
2020-06-23 23:47:19 +0300

    Username field added.

---
+++ Migrations/202006232044372_addUserName.cs
@@ -0,0 +1,16 @@
+namespace FilmBase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addUserName : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -49,6 +49,10 @@ namespace FilmBase.Models
     public class LoginViewModel
     {
         [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
         [Display(Name = "Email")]
         [EmailAddress]
         public string Email { get; set; }
@@ -64,6 +68,10 @@ namespace FilmBase.Models
 
     public class RegisterViewModel
     {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
