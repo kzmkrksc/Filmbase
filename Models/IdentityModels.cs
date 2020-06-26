ee90e39e348c11036047ea24a8426ad4f661c15f
Ahmet K. KARAKUSCU <22945971+kzmkrksc@users.noreply.github.com>
2020-06-26 23:02:33 +0300

    Movies VM added

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -21,6 +21,7 @@ namespace FilmBase.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
 
 
         public ApplicationDbContext()
