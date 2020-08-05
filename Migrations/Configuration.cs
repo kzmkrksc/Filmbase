00c306a0bf3ea2d75ce7c8b46c7cd96a34a6790a
ahmetkazim.karakuscu <ahmetkazim.karakuscu@csb.gov.tr>
2020-08-05 16:52:20 +0300

    database converted to LocalDb

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 namespace FilmBase.Migrations
 {
+    using FilmBase.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -19,6 +21,20 @@ namespace FilmBase.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+
+            var customers = new List<Customer>
+            {
+            new Customer{Name="Carson Alexander",IsSubscribedToNewsletter=false,MembershipTypeId=1,BirthDate=new DateTime(1989,11,11)},
+            new Customer{Name="Meredith Alonso",IsSubscribedToNewsletter=true,MembershipTypeId=2,BirthDate=new DateTime(1969,1,1)},
+            new Customer{Name="Arturo Anand",IsSubscribedToNewsletter=false,MembershipTypeId=3,BirthDate=new DateTime(1959,2,2)},
+            new Customer{Name="Gytis Barzdukas",IsSubscribedToNewsletter=true,MembershipTypeId=4,BirthDate=new DateTime(1939,3,3)},
+            new Customer{Name="Yan Li",IsSubscribedToNewsletter=false,MembershipTypeId=1,BirthDate=new DateTime(1988,11,21)},
+            new Customer{Name="Peggy Justice",IsSubscribedToNewsletter=true,MembershipTypeId=2,BirthDate=new DateTime(1985,6,14)},
+            new Customer{Name="Laura Norman",IsSubscribedToNewsletter=false,MembershipTypeId=3,BirthDate=new DateTime(1984,7,16)},
+            new Customer{Name="Nino Olivetto",IsSubscribedToNewsletter=true,MembershipTypeId=4,BirthDate=new DateTime(1982,8,1)}
+            };
+            customers.ForEach(s => context.Customers.Add(s));
+            context.SaveChanges();
         }
     }
 }
